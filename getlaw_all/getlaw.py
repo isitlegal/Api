ffd713e52af04f57359f71aada165c808c52d1bc
Heewoong Lee <37352488+PlanNoa@users.noreply.github.com>
2019-07-02 16:25:27 +0900

    Update getlaw.py

--- getlaw_all/getlaw.py
+++ getlaw_all/getlaw.py
@@ -115,7 +115,7 @@ def 자치법규():
 
 @app.route('/신규법령', methods = ['POST', 'GET'])
 def getnewlaws():
-    date = request.query_string.decode('utf-8')
+        date = request.query_string.decode('utf-8')
     url = 'http://www.law.go.kr/calendarInfoP.do?calDt=' + date
 
     delay = 3
@@ -130,19 +130,12 @@ def getnewlaws():
     driver.implicitly_wait(delay)
 
     json_data = OrderedDict()
-    json_data["data"] = []
-    json_data["data"].append({"분류": "시행법령"})
-    appendlaw(json_data, cleantext(driver.find_element_by_id('Si_tab').text))
-    json_data["data"].append({"분류": "공포법령"})
-    appendlaw(json_data, cleantext(driver.find_element_by_id('Gong_tab').text))
-    json_data["data"].append({"분류": "폐지법령"})
-    appendlaw(json_data, cleantext(driver.find_element_by_id('Abo_tab').text))
-    json_data["data"].append({"분류": "한시법령"})
-    appendlaw(json_data, cleantext(driver.find_element_by_id('Han_tab').text))
-    json_data["data"].append({"분류": "한시조문"})
-    appendlaw(json_data, cleantext(driver.find_element_by_id('Jo_tab').text))
-    json_data["data"].append({"분류": "위헌조문"})
-    appendlaw(json_data, cleantext(driver.find_element_by_id('Voc_tab').text))
+    json_data["Si"] = cleantext(driver.find_element_by_id('Si_tab').text)
+    json_data["Gong"] = cleantext(driver.find_element_by_id('Gong_tab').text)
+    json_data["Abo"] = cleantext(driver.find_element_by_id('Abo_tab').text)
+    json_data["Han"] = cleantext(driver.find_element_by_id('Han_tab').text)
+    json_data["Jo"] = cleantext(driver.find_element_by_id('Jo_tab').text)
+    json_data["Voc"] = cleantext(driver.find_element_by_id('Voc_tab').text)
 
     html = driver.page_source
     driver.quit()
@@ -158,12 +151,10 @@ def cleantext(text):
     department = []
     for law in text:
         law_split = law.split()
-        laws.append(' '.join(law_split[:-3]))
+        laws.append(" ".join(law_split[:-3]))
         kind.append(law_split[-3])
         number.append(law_split[-2])
         department.append(law_split[-1])
-
-    # return [laws, kind, number, department]
 
     res = []
     for law in zip(laws, kind, number, department):
@@ -175,11 +166,6 @@ def cleantext(text):
         res.append(temp)
 
     return res
-
-def appendlaw(json, laws):
-    for law in laws:
-        json["data"].append(law)
-    return json
 
 def printlaw(driver, search):
 
